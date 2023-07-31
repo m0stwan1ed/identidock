e8bd679fd5e2228d4490e35c68fe69d574328f37
Vlad Mostovych <m0stwan1ed@gmail.com>
2023-08-01 01:19:08 +0200

    uWSGI server prod has been fixed

--- cmd.sh
+++ cmd.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ "$ENV" = 'DEV' ]; then
     echo "Running development server"
-    exec python identidock.py
+    exec python /app/identidock.py
 elif [ "$ENV" = 'UNIT' ]; then
     echo "Running Unit Tests"
     exec python tests.py
 else
     echo "Running production server"
-    exec uwsgi --http 0.0.0.0:9090 -wsgi-file identidock.py --callable app --stats 0.0.0.0:9191
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
